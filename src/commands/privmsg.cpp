dc27fdeffef8d1da97cfd63d3b6afbbdb27f03dc
djimenezweb <djimenezweb@gmail.com>
2026-06-05 00:37:12 +0200

    parsing stream

--- src/commands/privmsg.cpp
+++ src/commands/privmsg.cpp
@@ -2,28 +2,79 @@
 
 void privmsg(Server &s, Client &c, std::string &line)
 {
-	std::cout << "PRIVMSG\n";
-	std::cout << "line =    '" << line << "'\n";
 	(void)s;
 	(void)c;
-	size_t space_pos = line.find(' ');
-	std::string targets = line.substr(0, space_pos);
-	std::cout << "targets = '" << targets << "'\n";
-	std::string text = line.substr(space_pos + 1);
-	std::cout << "text =    '" << text << "'\n";
 
+	std::istringstream iss1(line);
+	std::string text;
+	std::string targets_full;
+	std::vector<std::string> targets;
+
+	getline(iss1, targets_full, ' ');
+	getline(iss1 >> std::ws, text);
+	std::istringstream iss2(targets_full);
+	for (std::string t; getline(iss2, t, ',');)
+		targets.push_back(t);
+
+}
+
+/* #include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main()
+{
+	std::string line("manolo,gonzalo      mira que  bonita esta la luna");
+
+	std::istringstream iss1(line);
+	std::string targets;
 	std::vector<std::string> targets_v;
-	size_t start = space_pos + 1;
-	size_t found = targets.find(',');
-	while (found)
+	std::string text;
+
+	getline(iss1, targets, ' ');
+	getline(iss1 >> std::ws, text);
+	std::istringstream iss2(targets);
+	for (std::string target; getline(iss2, target, ',');)
+		targets_v.push_back(target);
+
+	std::cout << "targets: >" << targets << "<" << std::endl;
+	std::cout << "text:    >" << text << "<" << std::endl;
+	std::vector<std::string>::iterator it;
+	for (it = targets_v.begin(); it != targets_v.end(); it++)
+		std::cout << *it << std::endl;
+
+	return 0;
+}
+
+int main2()
+{
+	std::string line("manolo,gonzalo      mira que  bonita esta la luna");
+
+	std::istringstream input;
+	std::istringstream input2;
+	std::string targets;
+	std::string text;
+	std::vector<std::string> targets_v;
+
+	input.str(line);
+	getline(input, targets, ' ');
+	input >> std::ws;
+	getline(input, text);
+
+	std::cout << "targets: >" << targets << "<" << std::endl;
+	std::cout << "text:    >" << text << "<" << std::endl;
+
+
+	input2.str(targets);
+	for (std::string target; getline(input2, target, ',');)
 	{
-		targets_v.push_back(line.substr(start, found));
-		start = found + 1;
-		found = targets.find(',');
+		targets_v.push_back(target);
 	}
 
 	std::vector<std::string>::iterator it;
 	for (it = targets_v.begin(); it != targets_v.end(); it++)
 		std::cout << *it << std::endl;
 
-}
+	return 0;
+} */
