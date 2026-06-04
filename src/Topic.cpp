5f7199351cc6216112ad347b4b2dd873c7c84179
aldiaz-u <aldiaz-u>
2026-06-04 12:44:02 +0200

    fix topic

--- src/Topic.cpp
+++ src/Topic.cpp
@@ -13,30 +13,46 @@ void channelTopic(Server &s, Client &client, std::string line)
     std::map<std::string, Channel*>::iterator it = s.getChannels().find(channelName);
     if (it == s.getChannels().end())
     {
-        std::cout << "El canal no existe" << std::endl;
+        std::string reply = ":localhost 403 " + client.getNickname() + " " + channelName + " :No such channel\r\n";
+        print_message(client.getFd(), reply);
         return;
     }
 
     Channel &channel = (*it -> second);
-    std::vector<int> clients = channel.getClientsArray();
-    for (size_t i  = 0; i < clients.size(); i++)
+    if (!channel.hasClient(client))
     {
-        size_t index = 0;
-        int n_pos = 0;
-        bool flag = 0;
-        while (index < resto.length())
-        {
-            if (isspace(resto[index]) && flag == 0)
-                n_pos++;
-            else
-                flag = 1;
-            index++;
-        }
-        topic = resto.substr(n_pos);
-        if (client.getFd() == clients[i])
-            channel.setChannelTopic(topic);
-        else
-            print_message(client.getFd(), "El cliente no esta en el canal");
+        std::string reply = ":localhost 442 " + client.getNickname() + " " + channelName + " :You're not on that channel\r\n";
+        print_message(client.getFd(), reply);
+        return;
     }
-    std::cout << channel.getChannelName() <<" TOPIC: "<<channel.getChannelTopic() << std::endl;
+
+    size_t index = 0;
+    while (index < resto.length() && isspace(resto[index]))
+        index++;
+    if (index < resto.length() && resto[index] == ':')
+        index++;
+    topic = resto.substr(index);
+
+    if (topic.empty() && resto.find(':') == std::string::npos)
+    {
+        if (channel.getChannelTopic().empty())
+        {
+            std::string reply = ":localhost 331 " + client.getNickname() + " " + channelName + " :No topic is set\r\n";
+            print_message(client.getFd(), reply);
+        }
+        else
+        {
+            std::string reply = ":localhost 332 " + client.getNickname() + " " + channelName + " :" + channel.getChannelTopic() + "\r\n";
+            print_message(client.getFd(), reply);
+        }
+    }
+    else
+    {
+        channel.setChannelTopic(topic);
+        std::string broadcast = ":" + client.getNickname() + "!" + client.getUser() + "@localhost" + " TOPIC " + channelName + " :" + topic + "\r\n";
+        
+        std::vector<int> clients = channel.getClientsArray();
+        for (size_t i = 0; i < clients.size(); i++)
+            print_message(clients[i], broadcast);
+    }
 }
