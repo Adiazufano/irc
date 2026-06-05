992df0d393c50e6f1ef719f17b4e2cd0b154b39e
djimenezweb <djimenezweb@gmail.com>
2026-06-06 00:34:48 +0200

    working privmsg between users

--- src/commands/privmsg.cpp
+++ src/commands/privmsg.cpp
@@ -1,8 +1,21 @@
 #include "Server.hpp"
 
+/*
+# define ERR_NOSUCHNICK "401"
+# define ERR_CANNOTSENDTOCHAN "404"
+# define ERR_NOSUCHCHANNEL "403"
+# define ERR_NORECIPIENT "411"
+# define ERR_NOTEXTTOSEND "412"
+
+# define ERR_NOSUCHNICK(client, target) ("401 " + client + " " + target + " :No such nick/channel\r\n")
+# define ERR_NORECIPIENT(client) ("411 " + client + " :No recipient given PRIVMSG\r\n")
+# define ERR_NOTEXTTOSEND(client) ("412 " + client + " :No text to send\r\n")
+# define RPL_PRIVMSG(nick, username, target, message) (":" + nick + "!" + username + "@localhost PRIVMSG " + target + " " + message + "\r\n")
+*/
+
 void broadcastUser(Server &s, std::string &name, std::string &text)
 {
-	std::map<int, Client> clients = s.getClients();
+	std::map<int, Client> &clients = s.getClients();
 	std::map<int, Client>::iterator it;
 
 	for (it = clients.begin(); it != clients.end(); ++it)
@@ -15,41 +28,41 @@ void broadcastUser(Server &s, std::string &name, std::string &text)
 		// no such client
 		//ERR_NOSUCHNICK (401)
 		// "<client> <nickname> :No such nick/channel"
-
 	}
 }
 
 void broadcastChannel(Server &s, std::string &channelName, std::string &text, int exclude)
 {
 	// Check if user is member of the channel
+	// ERR_CANNOTSENDTOCHAN (404)
+	//"<client> <channel> :Cannot send to channel"
+
 	Channel *ch = s.getChannels()[channelName];
 	if (ch == NULL)
 	{
 		// no such channel
 		// ERR_NOSUCHCHANNEL (403)
 		// "<client> <channel> :No such channel"
-
 	}
 	std::vector<int> members = ch->getClientsArray();
-	for (int i = 0; i < members.size(); ++i)
+	for (size_t i = 0; i < members.size(); ++i)
 	{
 		if (members[i] != exclude)
 			print_message(members[i], text);
 	}
-	
 }
 
 void privmsg(Server &s, Client &c, std::string &line)
 {
-	(void)s;
-	(void)c;
+	std::cout << "privmsg: START - - - - - - - \n";
+	std::cout << "privmsg: line        : " << line << "\n";
 
 	std::istringstream iss1(line);
 	std::string text;
 	std::string targets_full;
 	std::vector<std::string> targets;
 
-	getline(iss1, targets_full, ' ');
+	getline(iss1 >> std::ws, targets_full, ' ');
 	getline(iss1 >> std::ws, text);
 
 	std::istringstream iss2(targets_full);
@@ -57,6 +70,7 @@ void privmsg(Server &s, Client &c, std::string &line)
 	{
 		targets.push_back(t);
 	}
+
 	if (text.empty())
 	{
 		// ERR_NOTEXTTOSEND (412) "<client> :No text to send"
@@ -70,78 +84,11 @@ void privmsg(Server &s, Client &c, std::string &line)
 			// ERR_NORECIPIENT (411) "<client> :No recipient given (<command>)"
 			std::string message = ":ircserver 411 " + c.getNickname() + " :No recipient given (PRIVMSG)";
 			print_message(c.getFd(), message);
+			//print_message(c.getFd(), std::string(":ircserver 411 " + c.getNickname() + " :No recipient given (PRIVMSG)"));
 		}
 		if (it->at(0) == '#')
-		{
 			broadcastChannel(s, *it, text, c.getFd());
-			//s.broadcastChannel(*it, c.getFd(), text);
-		}
 		else
-		{
 			broadcastUser(s, *it, text);
-		}
 	}
-	
-
 }
-
-/* #include <string>
-#include <iostream>
-#include <sstream>
-#include <vector>
-
-int main()
-{
-	std::string line("manolo,gonzalo      mira que  bonita esta la luna");
-
-	std::istringstream iss1(line);
-	std::string targets;
-	std::vector<std::string> targets_v;
-	std::string text;
-
-	getline(iss1, targets, ' ');
-	getline(iss1 >> std::ws, text);
-	std::istringstream iss2(targets);
-	for (std::string target; getline(iss2, target, ',');)
-		targets_v.push_back(target);
-
-	std::cout << "targets: >" << targets << "<" << std::endl;
-	std::cout << "text:    >" << text << "<" << std::endl;
-	std::vector<std::string>::iterator it;
-	for (it = targets_v.begin(); it != targets_v.end(); it++)
-		std::cout << *it << std::endl;
-
-	return 0;
-}
-
-int main2()
-{
-	std::string line("manolo,gonzalo      mira que  bonita esta la luna");
-
-	std::istringstream input;
-	std::istringstream input2;
-	std::string targets;
-	std::string text;
-	std::vector<std::string> targets_v;
-
-	input.str(line);
-	getline(input, targets, ' ');
-	input >> std::ws;
-	getline(input, text);
-
-	std::cout << "targets: >" << targets << "<" << std::endl;
-	std::cout << "text:    >" << text << "<" << std::endl;
-
-
-	input2.str(targets);
-	for (std::string target; getline(input2, target, ',');)
-	{
-		targets_v.push_back(target);
-	}
-
-	std::vector<std::string>::iterator it;
-	for (it = targets_v.begin(); it != targets_v.end(); it++)
-		std::cout << *it << std::endl;
-
-	return 0;
-} */
