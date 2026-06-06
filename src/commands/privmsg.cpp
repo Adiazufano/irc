b8c1977212bb310a43c5bae26ff5ee6b83391733
djimenezweb <djimenezweb@gmail.com>
2026-06-06 10:17:58 +0200

    privmsg working with error handling

--- src/commands/privmsg.cpp
+++ src/commands/privmsg.cpp
@@ -13,50 +13,66 @@
 # define RPL_PRIVMSG(nick, username, target, message) (":" + nick + "!" + username + "@localhost PRIVMSG " + target + " " + message + "\r\n")
 */
 
-void broadcastUser(Server &s, std::string &name, std::string &text)
+//:Angel PRIVMSG Wiz :Hello are you receiving this message ?
+//                                  ; Message from Angel to Wiz.
+
+void broadcastUser(Server &s, Client &c, std::string &target, std::string &text)
 {
 	std::map<int, Client> &clients = s.getClients();
 	std::map<int, Client>::iterator it;
 
+	// Iterate through clients map to find target
 	for (it = clients.begin(); it != clients.end(); ++it)
 	{
-		if (it->second.getNickname() == name)
-			print_message(it->first, text);
+		if (it->second.getNickname() == target)
+		{
+			std::string message = ":" + c.getNickname() + " PRIVMSG " + target + " :" + text;
+			std::cout << "Sending message to socket " << it->first << " " << message << "\n";
+			print_message(it->first, message);
+			return;
+		}
 	}
-	if (it == clients.end())
-	{
-		// no such client
-		//ERR_NOSUCHNICK (401)
-		// "<client> <nickname> :No such nick/channel"
-	}
+
+	// Target not found ERR_NOSUCHNICK (401) "<client> <nickname> :No such nick/channel"
+	std::string message = ":ircserver 401 " + c.getNickname() + " " + target + " :No such nick/channel";
+	print_message(c.getFd(), message);
 }
 
-void broadcastChannel(Server &s, std::string &channelName, std::string &text, int exclude)
+void broadcastChannel(Server &s, Client &c, std::string &channelName, std::string &text)
 {
-	// Check if user is member of the channel
-	// ERR_CANNOTSENDTOCHAN (404)
-	//"<client> <channel> :Cannot send to channel"
-
-	Channel *ch = s.getChannels()[channelName];
-	if (ch == NULL)
+	// Channel doesn't exist
+	if (!s.getChannels().count(channelName))
 	{
-		// no such channel
-		// ERR_NOSUCHCHANNEL (403)
-		// "<client> <channel> :No such channel"
+		// ERR_NOSUCHCHANNEL (403) "<client> <channel> :No such channel"
+		std::string message = ":ircserver 403 " + c.getNickname() + " " + channelName + " :No such channel";
+		print_message(c.getFd(), message);
+		return;
 	}
+
+	// Sender doesn't belong to channel
+	Channel *ch = s.getChannels()[channelName];
+	if (!ch->hasClient(c))
+	{
+		// ERR_CANNOTSENDTOCHAN (404) "<client> <channel> :Cannot send to channel"
+		std::string message = ":ircserver 404 " + c.getNickname() + " " + channelName + " :Cannot send to channel";
+		print_message(c.getFd(), message);
+		return;
+	}
+
+	// Send message to all channel members except sender
 	std::vector<int> members = ch->getClientsArray();
 	for (size_t i = 0; i < members.size(); ++i)
 	{
-		if (members[i] != exclude)
-			print_message(members[i], text);
+		if (members[i] == c.getFd())
+			continue;
+		std::string message = ":" + c.getNickname() + " PRIVMSG " + s.getClients()[i].getNickname() + " :" + text;
+		print_message(members[i], text);
 	}
 }
 
 void privmsg(Server &s, Client &c, std::string &line)
 {
-	std::cout << "privmsg: START - - - - - - - \n";
-	std::cout << "privmsg: line        : " << line << "\n";
-
+	// Parse targets and text
 	std::istringstream iss1(line);
 	std::string text;
 	std::string targets_full;
@@ -65,30 +81,32 @@ void privmsg(Server &s, Client &c, std::string &line)
 	getline(iss1 >> std::ws, targets_full, ' ');
 	getline(iss1 >> std::ws, text);
 
-	std::istringstream iss2(targets_full);
-	for (std::string t; getline(iss2, t, ',');)
-	{
-		targets.push_back(t);
-	}
-
+	// Message is empty
 	if (text.empty())
 	{
 		// ERR_NOTEXTTOSEND (412) "<client> :No text to send"
 		std::string message = ":ircserver 412 " + c.getNickname() + " :No text to send";
 		print_message(c.getFd(), message);
+		return;
 	}
-	for (std::vector<std::string>::iterator it = targets.begin(); it != targets.end(); ++it)
+
+	// Iterate through comma separated targets and send message to each one
+	std::istringstream iss2(targets_full);
+	for (std::string target; getline(iss2, target, ',');)
 	{
-		if (it->empty())
+		// Target is empty
+		if (target.empty())
 		{
 			// ERR_NORECIPIENT (411) "<client> :No recipient given (<command>)"
 			std::string message = ":ircserver 411 " + c.getNickname() + " :No recipient given (PRIVMSG)";
 			print_message(c.getFd(), message);
-			//print_message(c.getFd(), std::string(":ircserver 411 " + c.getNickname() + " :No recipient given (PRIVMSG)"));
+			return;
 		}
-		if (it->at(0) == '#')
-			broadcastChannel(s, *it, text, c.getFd());
+		// Target is a channel
+		if (target[0] == '#')
+			broadcastChannel(s, c, target, text);
+		// Target is a user
 		else
-			broadcastUser(s, *it, text);
+			broadcastUser(s, c, target, text);
 	}
 }
