e86fdfc0a8672a854b18c2d83dd0fa57a525a44b
aldiaz-u <aldiaz-u>
2026-06-08 19:34:55 +0200

    kick funciona

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -1,5 +1,6 @@
 #include "Server.hpp"
 #include "replies.hpp"
+#include <algorithm>
 
 void    commandKick(Server &s, Client &client, std::string line)
 {
@@ -15,36 +16,26 @@ void    commandKick(Server &s, Client &client, std::string line)
         print_message(client.getFd(), ERR_NEEDMOREPARAMS(client.getNickname(), "KICK"));
         return;
     }
-
     if (!checkName(channelName))
     {
         std::cout << "El nombre del canal no es correcto" << std::endl;
-        return ;
-    }
-
-    std::map<std::string, Channel*>& channels = s.getChannels(); 
-    if (channels.count(channelName) == 0)
-    {
-        std::cout << "Error: El canal " << channelName << " no existe" << std::endl;
         return;
     }
 
+    std::map<std::string, Channel*>& channels = s.getChannels();
     if (!s.getChannels().count(channelName))
-	{
-		// ERR_NOSUCHCHANNEL (403) "<client> <channel> :No such channel"
-		print_message(client.getFd(), ERR_NOSUCHCHANNEL(nick, channelName));
-		return;
-	}
+    {
+        print_message(client.getFd(), ERR_NOSUCHCHANNEL(client.getNickname(), channelName));
+        return;
+    }
 
-	Channel *channel = channels[channelName];
+    Channel *channel = channels[channelName];
     if (!channel->hasClient(client))
     {
-        // "<client> <channel> :You're not on that channel"
         print_message(client.getFd(), ERR_NOTONCHANNEL(client.getNickname(), channelName));
         return;
     }
-
-    if (!s.getChannels()[channelName]->isAdmin(client.getFd()))
+    if (!channel->isAdmin(client.getFd()))
     {
         print_message(client.getFd(), ERR_CHANOPRIVSNEEDED(client.getNickname(), channelName));
         return;
@@ -57,11 +48,20 @@ void    commandKick(Server &s, Client &client, std::string line)
         print_message(client.getFd(), ERR_USERNOTINCHANNEL(nick, channelName));
         return;
     }
-	Client &cli = s.getClients()[s.getClientsByNick()[nick]];
-    std::vector<int> _fd_clients = channel->getClientsArray();
-    if (channel -> hasClient(cli))
+
+    int targetFd = it->second;
+    if (s.getClients().count(targetFd) == 0)
     {
-        channel -> removeClient(cli);
+        print_message(client.getFd(), ERR_USERNOTINCHANNEL(nick, channelName));
+        return;
+    }
+
+    Client &cli = s.getClients()[targetFd];
+    std::vector<int> _fd_clients = channel->getClientsArray();
+    bool targetInChannel = std::find(_fd_clients.begin(), _fd_clients.end(), targetFd) != _fd_clients.end();
+
+    if (targetInChannel)
+    {
         if (!resto.empty() && resto[0] == ' ')
             resto.erase(0, 1);
         if (!resto.empty() && resto[0] == ':')
@@ -70,13 +70,15 @@ void    commandKick(Server &s, Client &client, std::string line)
             resto.erase(0, 1);
         if (resto.empty())
             resto = "Expulsado por un operador";
-        std::string msgKick = ":" + client.getNickname() + " KICK " + channelName + " " + nick + " :" + resto + "\r\n";
-        userMessages(s, cli, msgKick);
-        channel->removeClient(cli);
+
+        std::string msgKick = ":" + client.getNickname() + "!" + client.getUser() + "@" + client.getHostname() + " KICK " + channelName + " " + nick + " :" + resto;
+        for (size_t i = 0; i < _fd_clients.size(); ++i)
+            print_message(_fd_clients[i], msgKick);
+        channel->removeClient(cli.getFd());
+        cli.removeChannel(*channel);
     }
     else
     {
         print_message(client.getFd(), ERR_USERNOTINCHANNEL(nick, channelName));
     }
 }
-
