fcfbdb9cbefb734012150d2a86e3561be368cc13
Magin Parra Serrano <mparra-s@c2r8s3.42madrid.com>
2026-06-10 12:28:55 +0200

    sendMsg act

--- src/commands/cap.cpp
+++ src/commands/cap.cpp
@@ -4,29 +4,16 @@ void commandCap(std::istringstream &iss, Client &client)
 {
     std::string subcommand;
     if (!(iss >> subcommand))
-    {
-        print_message(client.getFd(), ":my_serv_irc 461 * CAP :Not enough parameters");
-        return;
-    }
+        return (client.sendMsg(ERR_NEEDMOREPARAMS(client.getNickname(), client.getCliCmd())));
     ft_toupper(subcommand);
     if (subcommand == "LS")
-    {
-        print_message(client.getFd(), ":my_serv_irc CAP * LS :");
-    }
+        client.sendMsg(":my_serv_irc CAP * LS :");
     else if (subcommand == "REQ")
     {
         std::string resto;
         std::getline(iss, resto);
-        print_message(client.getFd(), ":my_serv_irc CAP * NAK " +  resto);
+        client.sendMsg(":my_serv_irc CAP * NAK " +  resto);
     }
     else if (subcommand == "END")
-    {
-        /*if (!client.getHasPass())
-        {
-            print_message(client.getFd(), ":my_serv_irc 464 * :Password required");
-            return;
-        }*/
-        std::cout << "[SERVER] Negociación CAP finalizada." << std::endl;
         client.setAuthenticated(true);
-    }
 }
